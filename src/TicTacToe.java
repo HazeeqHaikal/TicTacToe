90675e73c88fdb0ffaf9cece272cb56d07b205a7
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-09-30 18:51:59 +0800

    first push

---
+++ src/TicTacToe.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class TicTacToe implements ActionListener {
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn;
+
+    TicTacToe(){
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+        frame.getContentPane().setBackground(new Color(50,50,50));
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        textfield.setBackground(new Color(25,25,25));
+        textfield.setForeground(new Color(25,255,0));
+        textfield.setFont(new Font("Ink Free",Font.BOLD,75));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("Tic-Tac-Toe");
+        textfield.setOpaque(true);
+
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0,0,800,100);
+
+        button_panel.setLayout(new GridLayout(3,3));
+
+        title_panel.add(textfield);
+        frame.add(title_panel,BorderLayout.NORTH);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    }
+
+    public void firstTurn(){
+
+    }
+
+    public void check(){
+
+    }
+
+    public void xWins(int a, int b, int c){
+
+    }
+
+    public void oWins(int a, int b, int c){
+
+    }
+
+
+}
